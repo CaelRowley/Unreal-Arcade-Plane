38dc030c7e606d4dd33e5718b54da79d4a7e4125
Cael Rowley <drdeus@pm.me>
2023-09-19 21:21:12 +0200

    feat: create template unreal project

---
+++ ArcadePlane/Source/ArcadePlane/ArcadePlane.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArcadePlane.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArcadePlane, "ArcadePlane" );
